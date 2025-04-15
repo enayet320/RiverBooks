1c5255d86fcdbb1a4a2161905dab1b6512c9bd02
MD Anayet Hossen <mdsakayethossen2@gmail.com>
2025-04-15 11:57:10 +0600

    Add project files.

---
+++ RiverBooks.Books/BookServiceExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using RiverBooks.Books.Data;
+
+namespace RiverBooks.Books;
+
+public static class BookServiceExtensions
+{
+  public static IServiceCollection AddBookServices(
+    this IServiceCollection services,
+    ConfigurationManager config)
+  {
+    string? connectionString = config.GetConnectionString("BooksConnectionString");
+    services.AddDbContext<BookDbContext>(options =>
+      options.UseSqlServer(connectionString, sqlOptions =>
+      {
+        sqlOptions.EnableRetryOnFailure(
+          maxRetryCount: 5,
+          maxRetryDelay: TimeSpan.FromSeconds(30),
+          errorNumbersToAdd: null);
+      }));
+    services.AddScoped<IBookRepository, EfBookRepository>();
+    services.AddScoped<IBookService, BookService>();
+    return services;
+  }
+}
